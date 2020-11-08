ac8a48f6febf844ce646472241e1758b38bb38cd
Monster880 <539561425@qq.com>
2020-11-08 14:01:02 +0800

    feat: 引入lodash

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@
 
 // import "@babel/polyfill";
 // babel window.Promise
-import { add } from './math.js';
+console.log(_.join(["a","d","c"],'***'));
+// 此处省略10万行业务逻辑
+console.log(_.join(["a","b","c"],'***'));
 
-add(1, 2);
+// 如果用户重新访问我们的页面，又要重新加载页面
